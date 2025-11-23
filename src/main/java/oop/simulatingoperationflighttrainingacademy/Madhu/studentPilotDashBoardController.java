b03fb0b14058944b6c40fb17c433645db0f699dd
Sabbir Islam <sabbirislamalvi070800@gmail.com>
2025-11-23 22:24:24 +0600

    Refactor dashboard and controller structure
    Updated HelloApplication to load the studentPilotDashBoard.fxml from the Madhu directory. Removed HelloController and hello-view.fxml, and added fx:controller to studentPilotDashBoard.fxml. Expanded studentPilotDashBoardController with FXML bindings and action methods. Added a new dashboard.fxml for Sabbir.

--- src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/studentPilotDashBoardController.java
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/studentPilotDashBoardController.java
@@ -1,7 +1,74 @@
 package oop.simulatingoperationflighttrainingacademy.Madhu;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class studentPilotDashBoardController
 {
     @javafx.fxml.FXML
+    private DatePicker dateDatePicker;
+    @javafx.fxml.FXML
+    private TreeTableView studentTableView;
+    @javafx.fxml.FXML
+    private ComboBox instructorComboBox;
+    @javafx.fxml.FXML
+    private TreeTableColumn DateTableColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn endTimeTableColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn instructorNameTableColumn;
+    @javafx.fxml.FXML
+    private TextField averageFlightTextField;
+    @javafx.fxml.FXML
+    private Label NotificationTextField;
+    @javafx.fxml.FXML
+    private TreeTableColumn startTimeTableColumn;
+    @javafx.fxml.FXML
+    private TextField totalFlightHourTextField;
+    @javafx.fxml.FXML
+    private TreeTableColumn aircraftModelTableColumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void resultAndCartificateOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void billOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logBookOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registrationOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void medicalCheckUpOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashBoardOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void selectSlotOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bookFlightOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void leaveOnActionButton(ActionEvent actionEvent) {
+    }
+}
