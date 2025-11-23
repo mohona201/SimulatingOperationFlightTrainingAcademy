7dad00d6c2dabbc7675b5c2d384678d7c7fa3c9e
Sabbir Islam <sabbirislamalvi070800@gmail.com>
2025-11-23 23:53:01 +0600

    Add logbook UI and controller for student pilot
    Introduced a new logbook interface in FXML with filtering and export options for student pilots. Added corresponding controller fields and navigation button handlers to support logbook access and related dashboard actions.

--- src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/logbookController.java
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/logbookController.java
@@ -1,7 +1,68 @@
 package oop.simulatingoperationflighttrainingacademy.Madhu;
-
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 public class logbookController
 {
     @javafx.fxml.FXML
+    private TextField filterFlightPathTextField;
+    @javafx.fxml.FXML
+    private TreeTableColumn instructorNameColumn;
+    @javafx.fxml.FXML
+    private TextField filterFlightTimeTextField;
+    @javafx.fxml.FXML
+    private TextField filterInstructorNameTextField;
+    @javafx.fxml.FXML
+    private TreeTableView logbookTreeTableView;
+    @javafx.fxml.FXML
+    private ComboBox filterAircraftModelComboBox;
+    @javafx.fxml.FXML
+    private TreeTableColumn flightPathColumn;
+    @javafx.fxml.FXML
+    private Button exportButton;
+    @javafx.fxml.FXML
+    private TreeTableColumn aircraftModelColumn;
+    @javafx.fxml.FXML
+    private DatePicker filterDatePicker;
+    @javafx.fxml.FXML
+    private TreeTableColumn dateColumn;
+    @javafx.fxml.FXML
+    private Button filterButton;
+    @javafx.fxml.FXML
+    private TreeTableColumn flightTimeColumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void resultAndCartificateOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void billOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logBookOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registrationOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void medicalCheckUpOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashBoardOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void leaveOnActionButton(ActionEvent actionEvent) {
+    }
+}
