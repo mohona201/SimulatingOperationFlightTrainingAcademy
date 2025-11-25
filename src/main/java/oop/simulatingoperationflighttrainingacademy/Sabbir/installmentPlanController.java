f03bdb6fecc48e2196d65d90b604c83a53447257
Sabbir Islam <sabbirislamalvi070800@gmail.com>
2025-11-25 12:14:14 +0600

    Add installment plan controller and FXML UI
    Introduces installmentPlanController.java with FXML bindings and event handlers for managing installment plans. Adds installmentPlan.fxml, a JavaFX UI for creating, assigning, and tracking installment plans and schedules for students.

---
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Sabbir/installmentPlanController.java
@@ -0,0 +1,134 @@
+package oop.simulatingoperationflighttrainingacademy.Sabbir;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class installmentPlanController
+{
+    @javafx.fxml.FXML
+    private DatePicker installmentFirstDueDatePicker;
+    @javafx.fxml.FXML
+    private TextField installmentStudentNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox installmentPlanStatusComboBox;
+    @javafx.fxml.FXML
+    private TreeTableColumn scheduleInstallmentNoColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn scheduleStudentNameColumn;
+    @javafx.fxml.FXML
+    private ComboBox installmentProgramComboBox;
+    @javafx.fxml.FXML
+    private Button saveInstallmentPlanButton;
+    @javafx.fxml.FXML
+    private Label installmentPlanStatusLabel;
+    @javafx.fxml.FXML
+    private Button exportInstallmentSchedulePdfButton;
+    @javafx.fxml.FXML
+    private Label assignInstallmentStatusLabel;
+    @javafx.fxml.FXML
+    private TextField installmentCountTextField;
+    @javafx.fxml.FXML
+    private Button markInstallmentPaidButton;
+    @javafx.fxml.FXML
+    private TextArea installmentPlanRemarksTextArea;
+    @javafx.fxml.FXML
+    private TextField installmentsDueThisWeekTextField;
+    @javafx.fxml.FXML
+    private Button markInstallmentOverdueButton;
+    @javafx.fxml.FXML
+    private TextField installmentStudentIdTextField;
+    @javafx.fxml.FXML
+    private ComboBox installmentPlanSelectComboBox;
+    @javafx.fxml.FXML
+    private TextField installmentPlanIdTextField;
+    @javafx.fxml.FXML
+    private ComboBox installmentFrequencyComboBox;
+    @javafx.fxml.FXML
+    private DatePicker installmentStartDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox installmentFeeTypeComboBox;
+    @javafx.fxml.FXML
+    private Button assignInstallmentPlanButton;
+    @javafx.fxml.FXML
+    private TreeTableView installmentScheduleTableView;
+    @javafx.fxml.FXML
+    private Label notificationLabel;
+    @javafx.fxml.FXML
+    private Button clearInstallmentPlanFormButton;
+    @javafx.fxml.FXML
+    private TreeTableColumn scheduleStatusColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn scheduleDueDateColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn scheduleAmountColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn schedulePlanIdColumn;
+    @javafx.fxml.FXML
+    private DatePicker installmentStudentStartDatePicker;
+    @javafx.fxml.FXML
+    private TextField installmentTotalAmountTextField;
+    @javafx.fxml.FXML
+    private TextField activeInstallmentPlansTextField;
+    @javafx.fxml.FXML
+    private TreeTableColumn scheduleStudentIdColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void scholarshipsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void installmentsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveInstallmentPlanOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportInstallmentSchedulePdfOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markInstallmentPaidOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearInstallmentPlanFormOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void refundsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void feesOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void suppliersOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignInstallmentPlanOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markInstallmentOverdueOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void paymentsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void holdsOnActionButton(ActionEvent actionEvent) {
+    }
+}
