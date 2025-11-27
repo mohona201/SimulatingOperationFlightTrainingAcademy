eab903b35d06dff89696afd7ee1a4a68bca95014
amanullahalmubin <mubingptplus@gmail.com>
2025-11-27 21:44:54 +0600

    Add Flight Instructor Dashboard UI and controller
    Introduces FlightInstructorDashboardController.java and its corresponding FXML layout for the flight instructor dashboard. The UI includes navigation buttons, student and session tables, and a notifications panel, providing the foundation for instructor-related features.

---
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Mubin/User5/FlightInstructorDashboardController.java
@@ -0,0 +1,61 @@
+package oop.simulatingoperationflighttrainingacademy.Mubin.User5;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+
+public class FlightInstructorDashboardController
+{
+    @javafx.fxml.FXML
+    private TreeTableColumn studentIdTableColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn aircraftTableColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn studentNameTableColumn;
+    @javafx.fxml.FXML
+    private TreeTableView sessionTableView;
+    @javafx.fxml.FXML
+    private TreeTableColumn dateTableColumn;
+    @javafx.fxml.FXML
+    private TreeTableColumn nextSessionTableColumn;
+    @javafx.fxml.FXML
+    private TextField upcomingSessionsTextField;
+    @javafx.fxml.FXML
+    private TreeTableColumn progressTableColumn;
+    @javafx.fxml.FXML
+    private TreeTableView studentsTableView;
+    @javafx.fxml.FXML
+    private TreeTableColumn timeTableColumn;
+    @javafx.fxml.FXML
+    private TextField assignedStudentsTextField;
+    @javafx.fxml.FXML
+    private TreeTableColumn sessionStudentTableColumn;
+    @javafx.fxml.FXML
+    private Label NotificationTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DashBoardOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sessionsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void studentsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportsOnActionButton(ActionEvent actionEvent) {
+    }
+}
