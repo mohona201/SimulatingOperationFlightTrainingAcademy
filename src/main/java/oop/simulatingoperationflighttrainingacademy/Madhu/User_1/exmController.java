d030b61afcde33b865e7da16ac325500f8eb06d5
mohona201 <madhumohona247@gmail.com>
2025-12-04 19:31:26 +0600

    Refactor student dashboard and exam controllers
    Updated method and variable names for consistency and clarity in studentPilotDashBoardController and exmController. Fixed FXML controller reference and button action handlers in studentPilotDashBoard.fxml to match refactored methods. Improved TableColumn property value factories and removed unused/duplicate code.

--- src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/User_1/exmController.java
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/User_1/exmController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import oop.simulatingoperationflighttrainingacademy.commonMethods;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -13,30 +14,31 @@ public class exmController {
     private TableColumn<Exm, String> instructorNameTableColumn;
     @javafx.fxml.FXML
     private TableColumn<Exm, LocalDateTime> startTimeTableColumn;
+    private TableColumn <Exm, LocalDate>endTimeTableColumn;
     @javafx.fxml.FXML
-    private TableView<Exm> theoreticalExamTableView;
+    private TableView <Exm>theoreticalExamTableView;
     @javafx.fxml.FXML
-    private TableColumn<Exm, String> examNameColumn;
+    private TableColumn <Exm,String>examNameColumn;
     @javafx.fxml.FXML
-    private TableColumn<Exm, LocalDate> endExmTableColumn;
+    private TableColumn <Exm,LocalDate>endExmTableColumn;
     @javafx.fxml.FXML
     private RadioButton practicalExam;
     @javafx.fxml.FXML
     private RadioButton theoreticalExam;
     @javafx.fxml.FXML
-    private TableColumn<Exm, LocalDate> startExmTableColumn;
+    private TableColumn <Exm,LocalDate>startExmTableColumn;
     @javafx.fxml.FXML
     private Label notificationLabel;
     @javafx.fxml.FXML
-    private TableColumn<Exm, LocalDate> exmEndTableColumn;
+    private TableColumn <Exm,LocalDate>exmEndTableColumn;
     @javafx.fxml.FXML
-    private TableColumn<Exm, Integer> examIDColumn;
+    private TableColumn <Exm,Integer>examIDColumn;
     @javafx.fxml.FXML
-    private TableColumn<Exm, String> theoryExamNameColumn;
+    private TableColumn <Exm,String>theoryExamNameColumn;
     @javafx.fxml.FXML
-    private TableColumn<Exm, String> roomTableColumn;
+    private TableColumn <Exm,String>roomTableColumn;
     @javafx.fxml.FXML
-    private TableView<Exm> practicalExamTableView;
+    private TableView <Exm>practicalExamTableView;
     @javafx.fxml.FXML
     private TextField examIDtextField;
     @javafx.fxml.FXML
@@ -46,21 +48,25 @@ public class exmController {
     @javafx.fxml.FXML
     private ToggleGroup examTypeToggleGroup;
     @javafx.fxml.FXML
-    private TableColumn<Exm, Integer> theoryExamIDColumn;
+    private TableColumn <Exm,Integer>theoryExamIDColumn;
 
     @javafx.fxml.FXML
     public void initialize() {
 
         startTimeTableColumn.setCellValueFactory(new PropertyValueFactory<>("startTime"));
         instructorNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("instructor"));
-        examIDColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        examNameColumn.setCellValueFactory(new PropertyValueFactory<>("exmName"));
-        exmEndTableColumn.setCellValueFactory(new PropertyValueFactory<>("exmEnd"));
-        startExmTableColumn.setCellValueFactory(new PropertyValueFactory<>("startExm"));
-        endExmTableColumn.setCellValueFactory(new PropertyValueFactory<>("exmEnd"));
-        roomTableColumn.setCellValueFactory(new PropertyValueFactory<>("room"));
-        theoryExamIDColumn.setCellValueFactory(new PropertyValueFactory<>("theoryId"));
-        theoryExamNameColumn.setCellValueFactory(new PropertyValueFactory<>("theoryExm"));
+
+        roomTableColumn.setCellValueFactory(new PropertyValueFactory<Exm,String>("room"));
+        theoryExamNameColumn.setCellValueFactory(new PropertyValueFactory<Exm,String>("theo ExmName"));
+        exmEndTableColumn.setCellValueFactory(new PropertyValueFactory<Exm,LocalDate>("endExm"));
+        startExmTableColumn.setCellValueFactory(new PropertyValueFactory<Exm,LocalDate>("startExm"));
+        endExmTableColumn.setCellValueFactory(new PropertyValueFactory<Exm,LocalDate>("exmEnd"));
+        examNameColumn.setCellValueFactory(new PropertyValueFactory<Exm,String>("exmName"));
+        endTimeTableColumn.setCellValueFactory(new PropertyValueFactory<Exm,LocalDate>("endTime"));
+
+
+         endTimeTableColumn.setCellValueFactory(new PropertyValueFactory<>("exmName"));
+
     }
 
     @javafx.fxml.FXML
@@ -88,21 +94,10 @@ public class exmController {
         commonMethods.sceneChange(actionEvent, "Madhu/User_1/studentPilotDashBoard.fxml");
     }
 
-    @javafx.fxml.FXML
-    public void theoreticalExamOnActionButton(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void applyExamOnActionButton(ActionEvent actionEvent) {
-    }
 
     @javafx.fxml.FXML
     public void scheduleOnActionButton(ActionEvent actionEvent) {
         commonMethods.sceneChange(actionEvent, "Madhu/User_1/trainingSession.fxml");
-    }
-
-    @javafx.fxml.FXML
-    public void practicalExamOnActionButton(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
@@ -113,5 +108,25 @@ public class exmController {
     @javafx.fxml.FXML
     public void leaveOnActionButton(ActionEvent actionEvent) {
         commonMethods.sceneChange(actionEvent, "Madhu/User_1/leave.fxml");
+    }
+
+    @Deprecated
+    public void selectSlotOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void bookFlightOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void theoreticalExamOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void applyExamOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void practicalExamOnActionButton(ActionEvent actionEvent) {
     }
 }
--- src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/User_1/studentPilotDashBoardController.java
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/User_1/studentPilotDashBoardController.java
@@ -4,79 +4,77 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import oop.simulatingoperationflighttrainingacademy.commonMethods;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class studentPilotDashBoardController
-{
+public class studentPilotDashBoardController {
+
     @javafx.fxml.FXML
     private DatePicker dateDatePicker;
     @javafx.fxml.FXML
-    private TableView <StudentPilot>studentTableView;
+    private TableView<StudentPilot> studentTableView;
     @javafx.fxml.FXML
-    private ComboBox <String>instructorComboBox;
+    private ComboBox<String> instructorComboBox;
     @javafx.fxml.FXML
-    private TableColumn <StudentPilot, LocalDateTime>endTimeTableColumn;
+    private TableColumn<StudentPilot, LocalDateTime> endTimeTableColumn;
     @javafx.fxml.FXML
-    private TableColumn <StudentPilot,String>instructorNameTableColumn;
+    private TableColumn<StudentPilot, String> instructorNameTableColumn;
     @javafx.fxml.FXML
     private TextField averageFlightTextField;
     @javafx.fxml.FXML
     private Label NotificationTextField;
     @javafx.fxml.FXML
-    private TableColumn <StudentPilot,LocalDateTime>startTimeTableColumn;
+    private TableColumn<StudentPilot, LocalDateTime> startTimeTableColumn;
     @javafx.fxml.FXML
     private TextField totalFlightHourTextField;
     @javafx.fxml.FXML
-    private TableColumn <StudentPilot,String>aircraftModelTableColumn;
+    private TableColumn<StudentPilot, String> aircraftModelTableColumn;
     @javafx.fxml.FXML
-    private TableColumn <StudentPilot,LocalDate>dateTableColumn;
+    private TableColumn<StudentPilot, LocalDate> dateTableColumn;
 
     @javafx.fxml.FXML
     public void initialize() {
-        instructorComboBox.getItems().addAll(" Md.Ahsan","phd Mostafizur","Ms Shila");
-        startTimeTableColumn.setCellValueFactory(new PropertyValueFactory<StudentPilot,LocalDateTime>("Start Time"));
-        aircraftModelTableColumn.setCellValueFactory(new PropertyValueFactory<StudentPilot,String>("air Craft Model"));
-        instructorNameTableColumn.setCellValueFactory(new PropertyValueFactory<StudentPilot,String>("Instructor Name"));
-        dateTableColumn.setCellValueFactory(new PropertyValueFactory<StudentPilot,LocalDate>("date"));
-        endTimeTableColumn.setCellValueFactory(new PropertyValueFactory<StudentPilot,LocalDateTime>("endTime"));
-        dateTableColumn.setCellValueFactory(new PropertyValueFactory<StudentPilot,LocalDate>("date"));
+        instructorComboBox.getItems().addAll("Md.Ahsan", "phd Mostafizur", "Ms Shila");
+        startTimeTableColumn.setCellValueFactory(new PropertyValueFactory<>("startTime"));
+        aircraftModelTableColumn.setCellValueFactory(new PropertyValueFactory<>("airCraftModel"));
+        instructorNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("instructorName"));
+        dateTableColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        endTimeTableColumn.setCellValueFactory(new PropertyValueFactory<>("endTime"));
     }
 
     @javafx.fxml.FXML
-    public void ScheduleOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/trainingSession.fxml");
+    public void scheduleOnActionButton(ActionEvent actionEvent) {
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/trainingSession.fxml");
     }
 
     @javafx.fxml.FXML
     public void resultAndCertificateOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/resultAndCertificate.fxml");
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/resultAndCertificate.fxml");
     }
 
     @javafx.fxml.FXML
     public void billOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/Bill.fxml");
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/bill.fxml");
     }
 
     @javafx.fxml.FXML
-    public void examsOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/exm.fxml");
+    public void exmOnActionButton(ActionEvent actionEvent) {
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/exm.fxml");
     }
 
     @javafx.fxml.FXML
     public void logBookOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/logbook");
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/logbook.fxml");
     }
 
     @javafx.fxml.FXML
     public void medicalCheckUpOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/medicalCheckUp.fxml");
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/medicalCheckUp.fxml");
     }
 
     @javafx.fxml.FXML
     public void dashBoardOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/studentPilotDashboard.fxml");
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/studentPilotDashBoard.fxml");
     }
 
     @javafx.fxml.FXML
@@ -89,6 +87,6 @@ public class studentPilotDashBoardController
 
     @javafx.fxml.FXML
     public void leaveOnActionButton(ActionEvent actionEvent) {
-        commonMethods.sceneChange(actionEvent,"Madhu/User_1/leave.fxml");
+        commonMethods.sceneChange(actionEvent, "Madhu/User_1/leave.fxml");
     }
 }
