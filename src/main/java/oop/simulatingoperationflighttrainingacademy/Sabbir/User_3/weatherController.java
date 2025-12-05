2729c530d71f5db15598c5229a7bb77db05f4536
amanullahalmubin <mubingptplus@gmail.com>
2025-12-05 19:29:30 +0600

    Add weather controller and FXML UI for login, signup, and weather report
    Introduced weatherController.java for managing weather-related UI logic. Added FXML files for login, signup, and weather report screens, providing user interfaces for authentication and weather monitoring in the flight training academy application.

---
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Sabbir/User_3/weatherController.java
@@ -0,0 +1,30 @@
+package oop.simulatingoperationflighttrainingacademy.Sabbir.User_3;
+
+public class weatherController
+{
+    @javafx.fxml.FXML
+    private Label NameView;
+    @javafx.fxml.FXML
+    private TableColumn airportCodeColumn;
+    @javafx.fxml.FXML
+    private Label notificationLabel;
+    @javafx.fxml.FXML
+    private TableColumn temperatureColumn;
+    @javafx.fxml.FXML
+    private TextField temperatureField;
+    @javafx.fxml.FXML
+    private TableView weatherTableView;
+    @javafx.fxml.FXML
+    private TableColumn remarksColumn;
+    @javafx.fxml.FXML
+    private TextField windSpeedField;
+    @javafx.fxml.FXML
+    private TableColumn windColumn;
+    @javafx.fxml.FXML
+    private TextField visibilityField;
+    @javafx.fxml.FXML
+    private TableColumn conditionColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
