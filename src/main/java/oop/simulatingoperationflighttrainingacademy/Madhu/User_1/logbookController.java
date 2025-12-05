a95556b7ffd886ffb61cd2e8e2a5ca91ce14bf2b
mohona201 <madhumohona247@gmail.com>
2025-12-05 20:52:04 +0600

    Update logbook filter logic and add theoretical data
    Refactored logbookController to always load full data before filtering and clear table items before applying filters. Added new theoretical.bin data file and updated medical.bin and user.bin binary data files.

--- src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/User_1/logbookController.java
+++ src/main/java/oop/simulatingoperationflighttrainingacademy/Madhu/User_1/logbookController.java
@@ -58,14 +58,16 @@ public class logbookController {
         LocalDate date = dateDatePickerComboBox.getValue();
 
         if (instructor.isEmpty() && flightPath.isEmpty() && model == null && date == null) {
-
             commonMethods.showError("Empty Filter", "Please enter at least one filter");
             notificationLabel.setText("Fields are empty");
             return;
         }
 
+        // Always load original full data
         ArrayList<LogBook> all = new ArrayList<>();
+        commonMethods.showTableDataFromBinFile("logbook.bin", flightBookTableView);
         all.addAll(flightBookTableView.getItems());
+        flightBookTableView.getItems().clear();
 
         ArrayList<LogBook> filtered = new ArrayList<>();
 
